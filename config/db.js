18f811e39fe23097c3a6ac0ac18bb3d6d4c0e3f3
Javier Zabala <javier_bislip@hotmail.com>
2026-06-03 19:56:38 -0300

    Revert "Force create collections"
    This reverts commit fae168aa745d86fb6f9b3cdab15913a18e2e1a48.

--- config/db.js
+++ config/db.js
@@ -1,19 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    })
-    .then(async () => {
-      const models = Object.keys(mongoose.models);
-      for (const modelName of models) {
-        await mongoose.models[modelName].createCollection();
-      }
-    });
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  });
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
