b649f668fd452e5af07a50e541e938dd69e2eedc
Javier Zabala <javier_bislip@hotmail.com>
2026-06-03 19:39:37 -0300

    Update app URLs

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,6 +3,6 @@ import socketIOClient from "socket.io-client";
 const endpoint =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5001"
-    : "https://ml-crowdsourcing-platform.vercel.app";
+    : "https://ml-crowdsourcing-platform.onrender.com";
 
 export const socket = socketIOClient(endpoint);
--- client/src/store/axios.js
+++ client/src/store/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5001/api"
-    : "https://ml-crowdsourcing-platform.vercel.app/api";
+    : "https://ml-crowdsourcing-platform.onrender.com/api";
 
 const axiosInstance = axios.create({ baseURL });
 
