ae615384fdd034cbb7b9978269d2b49ae7a0685f
Javier Zabala <javier_bislip@hotmail.com>
2026-06-03 16:36:02 -0300

    Create index file for registering schemas

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const Admin = require("./Admin");
+const Contributor = require("./Contributor");
+const Counter = require("./Counter");
+const Data = require("./Data");
+const Dataset = require("./Dataset");
+const Global = require("./Global");
+const Log = require("./Log");
+const Model = require("./Model");
+const Question = require("./Question");
+const Task = require("./Task");
+const User = require("./User");
+
+module.exports = {
+  Admin,
+  Contributor,
+  Counter,
+  Data,
+  Dataset,
+  Global,
+  Log,
+  Model,
+  Question,
+  Task,
+  User,
+};
