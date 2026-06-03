fb72996b93d2ee481936f45fe66a5b9c24d642a7
Javier Zabala <javier_bislip@hotmail.com>
2026-06-03 15:48:03 -0300

    Update client API URL

--- client/src/store/axios.js
+++ client/src/store/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5001/api"
-    : "https://ml-crowdsourcing-platform.herokuapp.com/api";
+    : "https://ml-crowdsourcing-platform.vercel.app/api";
 
 const axiosInstance = axios.create({ baseURL });
 
